3c8f206840cbe28ac1aeb0a9921af11b29bdb5a6
Krzysztof Wojciechowski <krzy.wojciecho@gmail.com>
2026-08-26 22:59:02 +0200

    Add lut transmission with simulated actuator | Fix observation space of simple model | Tune constraints

--- src/pal_mjlab/tasks/velocity/kangaroo_full/__init__.py
+++ src/pal_mjlab/tasks/velocity/kangaroo_full/__init__.py
@@ -17,11 +17,11 @@ register_mjlab_task(
 
 for state in ("full_state", "actuator_space", "simple_model"):
   for pose_space in (False, True):
-    for pd_mapping in ("jacobian", "lowest"):
+    for pd_mapping in ("jacobian", "lowest", "semi_serial"):
       task_id_base = "Mjlab-Velocity-Flat-Pal-Kangaroo-Full"
       state_name = state.replace("_", " ").title().replace(" ", "-")
       pose_space_name = "-Pose-In-Actuator" if pose_space else ""
-      pd_map_name = pd_mapping.title()
+      pd_map_name = pd_mapping.replace("_", " ").title().replace(" ", "-")
       register_mjlab_task(
         task_id=f"{task_id_base}-{state_name}-PD-{pd_map_name}{pose_space_name}",
         env_cfg=pal_kangaroo_full_flat_env_cfg(
