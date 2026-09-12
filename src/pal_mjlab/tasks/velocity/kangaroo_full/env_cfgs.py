5301e4d6cac572bbbda40a3539d087434450485f
Krzysztof Wojciechowski <krzy.wojciecho@gmail.com>
2026-09-12 21:18:46 +0200

    Change nconmax

--- src/pal_mjlab/tasks/velocity/kangaroo_full/env_cfgs.py
+++ src/pal_mjlab/tasks/velocity/kangaroo_full/env_cfgs.py
@@ -53,7 +53,7 @@ def pal_kangaroo_full_rough_env_cfg(
 ) -> ManagerBasedRlEnvCfg:
   """Create PAL Robotics KANGAROO FULL rough terrain velocity configuration."""
   cfg = pal_kangaroo_baseline_env_cfg(play)
-  cfg.sim.nconmax = 70
+  cfg.sim.nconmax = 200
 
   model = get_kangaroo_full_model(
     hip_z=hip_z,
